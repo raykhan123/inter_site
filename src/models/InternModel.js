0765f5cfbbcfcecab2fdff35e8f41d06bf1aa4fc
imtiyazz24 <imtiyaz.ansari24@gmail.com>
2022-06-28 16:37:36 +0530

    y

---
+++ src/models/InternModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const internSchema = new mongoose.Schema({
+
+
+
+    
+
+
+
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('College', internSchema)
---
+++ src/models/collegeModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const collegeSchema = new mongoose.Schema({
+
+
+
+
+    
+
+
+
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('College', collegeSchema)
