20e490e0339afc92a65151c946f42584a4974cd0
imtiyazz24 <104253084+imtiyazz24@users.noreply.github.com>
2022-06-28 19:30:12 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const collegeController = require("../controllers/collegeController")
 const interController = require("../controllers/internController")
 
-<<<<<<< HEAD
+
 router.post("/functionup/colleges", authorController.createAutho)
-=======
+
 router.post("/functionup/colleges",collegeController.createCollege)
->>>>>>> cab49fe8c3545f7e5c371d8e8637e9a68cd91ab7
+
 
 router.post("/functionup/interns",interController.createintern)
 
