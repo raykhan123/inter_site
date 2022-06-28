0b50e609e24547450a7ffc74134290cb266f6817
imtiyazz24 <imtiyaz.ansari24@gmail.com>
2022-06-28 17:15:05 +0530

    controller

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,2 +1,17 @@
 const collegeModel = require("../models/collegeModel")
 const interModel = require("../models/InternModel")
+
+const createCollege = async function(req,res){
+    try{
+        let data = req.body
+        let saveData = await collegeModel.create(data)
+        res.status(200).send({status:true, data:saveData})
+    }
+    catch(error){
+        console.log(error.message)
+        res.satus(500).send({status:false, msg: error.message})
+
+    }
+}
+
+module.exports={createCollege}
